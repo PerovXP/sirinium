649224cc889397721952efdcdf3644776e9ce3d5
Daniil Perov <danilperov796@gmail.com>
2025-02-25 12:17:59 +0300

    Add parser wrapper

--- client/index.js
+++ client/index.js
@@ -1,108 +1,37 @@
-const utils = require("./utils");
+const parser = require("./parser");
 
 class Client {
-    constructor(url = "https://schedule.siriusuniversity.ru") {
-        this.url = url;
-        this.mainGridUrl = `${this.url}/livewire/message/main-grid`;
+    constructor(options = {}) {
+        this.options = {
+            domain: options.domain ?? "https://schedule.siriusuniversity.ru",
+            mainGridUrl: options.mainGridUrl ?? undefined
+        };
+
+        this.parser = new parser(this.options.domain, this.options.mainGridUrl);
     }
 
     async getInitialData() {
-        const response = await fetch(this.url, { credentials: "same-origin" });
-
-        this.xsrfToken = await utils.getXsrfToken(response).catch((e) => {
-            return new Error(`Can't get xsrf token: ${e.message}`);
-        });
-        this.sessionToken = await utils.getSessionToken(response).catch((e) => {
-            return new Error(`Can't get session token: ${e.message}`);
+        await this.parser.getInitialData().catch((e) => {
+            throw new Error("Can't get inital data: " + e);
         });
 
-        const body = await response.text();
-
-        const initialData = await utils.parseInitialData(body).catch((e) => {
-            return new Error(`Can't parse initial data: ${e.message}`);
-        });
-        this.data = initialData;
-
-        this.wireToken = await utils.getWireToken(body).catch((e) => {
-            return new Error(`Can't get wire token: ${e.message}`);
-        });
-
-        await this.emulateResize(1920, 1080); // Redundant, but why not?
-
-        return initialData;
+        return true;
     }
 
-    async getGroupSchedule(num) {
-        const data = await this.sendUpdates(
-            [utils.getCallMethodUpdateObject("set", [num])]
-        );
-
-        return await utils.getArrayOfEvents(data).catch((e) => {
-            return new Error(`Can't get array of events: ${e.message}`);
-        });
-    }
-
-    async emulateResize(width, height) {
-        const data = await this.sendUpdates([
-            utils.getCallMethodUpdateObject("render"),
-            utils.getCallMethodUpdateObject("$set", ["width", width]),
-            utils.getCallMethodUpdateObject("$set", ["height", height]),
-        ]).catch((e) => {
-            return new Error(`Can't send updates: ${e.message}`);
+    async getGroupSchedule(group) {
+        const schedule = await this.parser.getGroupSchedule(group).catch((e) => {
+            throw new Error("Can't get group schedule: " + e);
         });
 
-        this.data.serverMemo.data.width = data.serverMemo.data.width;
-        this.data.serverMemo.data.height = data.serverMemo.data.height;
-        this.data.serverMemo.checksum = data.serverMemo.checksum;
-
-        return data;
+        return schedule;
     }
 
-    async changeWeek(action) {
-        const data = await this.sendUpdates([
-            utils.getCallMethodUpdateObject(action === "add" ? "addWeek" : "minusWeek")
-        ], true).catch((e) => {
-            return new Error(`Can't send updates: ${e.message}`)
+    async changeWeek(step) {
+        if (Number.isInteger(step) || step === 0) return;
+
+        await this.parser.changeWeek(step).catch((e) => {
+            throw new Error("Can't change week: " + e);
         });
-
-        Object.assign(this.data.serverMemo.data, data.serverMemo.data);
-
-        this.data.serverMemo.checksum = data.serverMemo.checksum;
-        this.data.serverMemo.htmlHash = data.serverMemo.htmlHash;
-
-        return data;
-    }
-
-    async sendUpdates(updates) {
-        const request = await fetch(this.mainGridUrl, {
-            method: "POST",
-            credentials: "same-origin",
-            headers: this.getHeaders(),
-            body: JSON.stringify({
-                ...this.getInitialBody(),
-                updates: updates
-            })
-        });
-
-        return await request.json();
-    }
-
-    getInitialBody() {
-        return {
-            fingerprint: this.data["fingerprint"],
-            serverMemo: this.data["serverMemo"]
-        };
-    }
-
-    getHeaders() {
-        return {
-            "Cookie": `XSRF-TOKEN=${this.xsrfToken};raspisanie_universitet_sirius_session=${this.sessionToken}`,
-
-            "X-Livewire": "true",
-            "X-Csrf-Token": this.wireToken ?? "",
-
-            "Content-Type": "application/json"
-        }
     }
 }
 
---
+++ client/parser.js
@@ -0,0 +1,96 @@
+const utils = require("./utils");
+
+class Parser {
+    constructor(domain, mainGrid) {
+        this.url = domain;
+        this.mainGridUrl = mainGrid ?? `${this.url}/livewire/message/main-grid`;
+    }
+
+    async getInitialData() {
+        const response = await fetch(this.url, { credentials: "same-origin" });
+
+        this.xsrfToken = await utils.getXsrfToken(response);
+        this.sessionToken = await utils.getSessionToken(response);
+
+        const body = await response.text();
+
+        const initialData = await utils.parseInitialData(body);
+        this.data = initialData;
+
+        this.wireToken = await utils.getWireToken(body);
+
+        await this.emulateResize(1920, 1080); // Redundant, but why not?
+
+        return initialData;
+    }
+
+    async getGroupSchedule(group) {
+        const data = await this.sendUpdates(
+            [utils.getCallMethodUpdateObject("set", [group])]
+        );
+
+        return await utils.getArrayOfEvents(data);
+    }
+
+    async emulateResize(width, height) {
+        const data = await this.sendUpdates([
+            utils.getCallMethodUpdateObject("render"),
+            utils.getCallMethodUpdateObject("$set", ["width", width]),
+            utils.getCallMethodUpdateObject("$set", ["height", height]),
+        ]);
+
+        this.data.serverMemo.data.width = data.serverMemo.data.width;
+        this.data.serverMemo.data.height = data.serverMemo.data.height;
+        this.data.serverMemo.checksum = data.serverMemo.checksum;
+
+        return true;
+    }
+
+    async changeWeek(step) {
+        const method = step > 0 ? "addWeek" : "minusWeek";
+        for (let i = 0; i <= Math.abs(step); i++) {
+            const data = await this.sendUpdates([utils.getCallMethodUpdateObject(method)]);
+
+            Object.assign(this.data.serverMemo.data, data.serverMemo.data);
+
+            this.data.serverMemo.checksum = data.serverMemo.checksum;
+            this.data.serverMemo.htmlHash = data.serverMemo.htmlHash;
+        }
+
+        return true;
+    }
+
+    async sendUpdates(updates) {
+        const data = await fetch(this.mainGridUrl, {
+            method: "POST",
+            credentials: "same-origin",
+            headers: this.getHeaders(),
+            body: JSON.stringify({
+                ...this.getInitialBody(),
+                updates: updates
+            })
+        });
+
+        return await data.json();
+    }
+
+    getInitialBody() {
+        return {
+            fingerprint: this.data["fingerprint"],
+            serverMemo: this.data["serverMemo"]
+        };
+    }
+
+    getHeaders() {
+        return {
+            "Cookie": `XSRF-TOKEN=${this.xsrfToken};raspisanie_universitet_sirius_session=${this.sessionToken}`,
+
+            "X-Livewire": "true",
+            "X-Csrf-Token": this.wireToken ?? "",
+
+            "Content-Type": "application/json"
+        }
+    }
+}
+
+module.exports = Parser;
